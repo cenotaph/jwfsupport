72e1d167b4f07c0d484ee56d9b04ef55bedc0c04
John W. Fail <john@pixelache.ac>
2017-02-05 16:56:07 +0200

    project view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,6 +4,8 @@ class ProjectsController < ApplicationController
     @project = Project.friendly.find(params[:id])
     if @project.users.include?(current_user) || current_user.has_role?(:admin)
       @tickets = @project.tickets
+      # @progress_total = (Ticket.closed.size.to_f / Ticket.all.size.to_f).to_f * 100
+      @progress_project = (@project.tickets.closed.size.to_f / @project.tickets.size.to_f).to_f * 100
     else
       flash[:notice] = 'You do not have permission to view this project.'
       redirect_to '/'
